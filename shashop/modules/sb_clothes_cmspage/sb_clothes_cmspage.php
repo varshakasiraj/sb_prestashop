9f61bd99426369c01bc6caef514c005c56f49bc5
varshakasiraj <116131392+varshakasiraj@users.noreply.github.com>
2023-09-08 18:17:10 +0530

    adjust(cmspage)

--- shashop/modules/sb_clothes_cmspage/sb_clothes_cmspage.php
+++ shashop/modules/sb_clothes_cmspage/sb_clothes_cmspage.php
@@ -12,7 +12,12 @@ class sb_clothes_cmspage extends Module{
     }
     public function install()
     {
-        return parent::install();
+        return parent::install()&&
+        $this->registerHook('displayHeader')&&
+        $this->registerHook('displayHome')&&
+        $this->registerHook('actionCmsPageFormBuilderModifier')&&
+        $this->registerHook('actionAfterCreateCmsPageFormHandler')&&
+        $this->registerHook('actionAfterUpdateCmsPageFormHandler');
     }
     public function uninstall(){
         return parent::uninstall();
@@ -31,16 +36,51 @@ class sb_clothes_cmspage extends Module{
             );
         return  $this->display(__FILE__, 'sb_clothes_cmspage.tpl');
     }
-   
+    public function hookActionCmsPageFormBuilderModifier($params){
+        $formBuilder = $params['form_builder'];
+        $allFields = $formBuilder->all();
+        foreach ($allFields as  $inputField => $input) {
+            $formBuilder->add($input);
+            $formBuilder->add(
+                'cover_image', 
+                FileType::class, 
+                ['label' => 'coverimage']
+            );
+        }
+    }
+    public function hookActionAfterUpdateCmsPageFormHandler($params){
+        $this->insertImageToDb($params["id"]);
+    }
+
+    public function hookActionAfterCreateCmsPageFormHandler($params){
+       $this->insertImageToDb($params["id"]);
+    }
+    public function insertImageToDb($id){
+        global $_POST;
+        global $asset_uri, $error,$errors,$img_path;
+        $allowed_img_extension = array("png", "jpg", "jpeg", "jfif");
+        $basename = basename($_FILES['cms_page']['name']['cover_image']);
+        $extension = substr(strrchr($basename, '.'), 1);
+        if (in_array($extension, $allowed_img_extension)) {
+            $path =$_FILES['cms_page']['tmp_name']['cover_image'];
+            $image_path = "C:\\xamp\\xampp\htdocs\shashop\img\cms\\".$basename;
+            move_uploaded_file($path, $image_path);
+            $cms = new CMS($id);
+            $cms->cover_image = $basename;
+            $cms->update();
+        } 
+    }
     public function getContent(){
         return $this->display(__FILE__, 'sb_clothes_cmspage.tpl');
     }
     public function getCmsCategory(){
         $cmscategory = new CMS();
+        $link = new Link();
+     
         foreach($cmscategory->getCMSPages(1,6) as $cmspages){
             $cmspagesdetails[] =[
                 "title"=>$cmspages["head_seo_title"],
-                "link_rewrite" => $cmspages["link_rewrite"],
+                "link_rewrite" => $link->getCMSLink($cmspages['id_cms']),
                 "cover_image" =>$cmspages["cover_image"]
             ];
         }
--- shashop/modules/sb_clothes_cmspage/sb_clothes_cmspage.tpl
+++ shashop/modules/sb_clothes_cmspage/sb_clothes_cmspage.tpl
@@ -5,10 +5,7 @@
         {foreach $sliders as $slider}
             <div class="sb_clothes_cmspage_card one">
                     <div class="sb_clothes_cmspage_image">
-                        <a href="{$slider["link_rewrite"]}"><img src="C:\\xamp\\xampp\htdocs\shashop\img\cms\\{$slider["cover_image"]}"
-                        
-                        /></a>
-                        {var_dump("C:\\xamp\\xampp\\htdocs\\shashop\\img\\cms\\{$slider["cover_image"]}")}
+                     <a href="{$slider["link_rewrite"]}"> <img src="/shashop/img/cms/{$slider["cover_image"]}"/></a>  
                     </div>
                     <div class="sb_clothes_cmspage_description 1">
                         <h5>{$slider["title"]}</h5>
